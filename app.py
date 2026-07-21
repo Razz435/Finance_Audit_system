5ca6be39e77ae996c729cc00bf6aa0bb5c95f417
Rahul Raj Sah <157842461+Razz435@users.noreply.github.com>
2026-07-22 01:20:09 +0530

    Expose app globally and update Procfile to avoid bash syntax error

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ def create_app():
     
     return app
 
+app = create_app()
+
 if __name__ == '__main__':
-    app = create_app()
-    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=False)
+    app.run(debug=True, host='0.0.0.0', port=5000)
